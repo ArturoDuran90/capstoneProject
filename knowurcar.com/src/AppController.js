8a32b094ac7407f4f08f13838dfc49b35b92c74e
ArturoDuran90 <aduran@student.neumont.edu>
2022-07-13 17:57:28 -0600

    changes

--- knowurcar.com/src/AppController.js
+++ knowurcar.com/src/AppController.js
@@ -43,8 +43,13 @@ function AppController() {
                     <div className='box'>
                         <img className="imgCar" src={car1} alt='Vehicle Poster'/>
                     </div>
-                </div>   
-                
+                    <h2>BMW 328i xDrive GT</h2>
+                    <div className="vehiclesInfo">
+                        carType: "Mid-Size Sedan", "\n"
+                        "carDrivetrain": "AWD",
+                        "carPrice": "$43,000"
+                    </div>
+                </div>
                 <div id='vehicleFav2Box'className='box'>
                     <div id="personalFav" className='box'>Our Personal Favorites</div>
                     
@@ -52,12 +57,14 @@ function AppController() {
                         <div className='box'>
                             <img className="imgCar" src={car2} alt='Vehicle Poster'/>
                         </div>
+                        <h2>Kia K5 GT-Line</h2>
                     </div>
                 </div>
                 <div id='vehicleFav3' className='box'>
                     <div className='box'>
                         <img className="imgCar" src={car3} alt='Vehicle Poster'/>
                     </div>
+                    <h2>Honda Civic Type R</h2>
                 </div>
             </div>
             
