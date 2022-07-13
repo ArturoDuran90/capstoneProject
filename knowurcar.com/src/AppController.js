216f222cb2bb09a3fb13651e2e602bbb52681fd4
ArturoDuran90 <aduran@student.neumont.edu>
2022-07-12 18:27:15 -0600

    continue working on landing page

--- knowurcar.com/src/AppController.js
+++ knowurcar.com/src/AppController.js
@@ -1,5 +1,8 @@
 import './App.css';
 import LightsTable from './LightsTable';
+import car1 from './images/car1.jpg';
+import car2 from './images/car2.jpg';
+import car3 from './images/car3.jpg';
 
 const refreshPage = ()=>{
     window.location.reload();
@@ -7,94 +10,65 @@ const refreshPage = ()=>{
 
 function AppController() {
     return (
-    <div className="App">
-        <div id='container' class='box'>
-            <div id='titleBox' class='box'>
+    // <div className="App">
+        <div id='container' className='box'>
+            <div id='titleBox' className='box'>
                 <img onClick={() => {refreshPage()}} id="pageLogo" src="KUClogo.png" alt="logo"/>
             </div>
         
-            <div id='box2' class='box'>
-                <div class='box'>
-                    <input class='box' type='text' placeholder='Vehicle Year'/>
-                    {/* <a href={carWebsite(sedan.carModel)} target="_blank">
-                        <img src={sedan.carImage} alt='Vehicle Poster'/>
-                    </a> */}
+            <div id='navBar' className='box'>
+                <div className='box'>
+                    <input className='box vehicleInput' type='text' placeholder='Vehicle Year'/>
                 </div>
-                <div class='box'>
-                    <input class='box' type='text' placeholder='Vehicle Make'/>
+                <div className='box'>
+                    <input className='box vehicleInput' type='text' placeholder='Vehicle Make'/>
                 </div>
-                <div class='box'>
-                    <input class='box' type='text' placeholder='Vehicle Model'/>
+                <div className='box'>
+                    <input className='box vehicleInput' type='text' placeholder='Vehicle Model'/>
                 </div>
-                <div class='box'>
-                    <input class='box' type='text' placeholder='Vehicle Trim'/>
+                <div className='box'>
+                    <input className='box vehicleInput' type='text' placeholder='Vehicle Trim'/>
                 </div>
             </div>
 
-            <div id='box5' class='box'>
+            <div id='box5' className='box'>
                 <div id='lookUpBtn'>
                     Look up Vehicle
-                    {/* <input class='box' type='button' placeholder='Look up Vehicle'/> */}
                 </div>
             </div>
 
-            <div id='box3' class='box'>
-                <div id= 'vehicleFav1' class='box'>
-                    Car 1
+            <div id='vehicleFavContainer' className='box'>
+                <div id='vehicleFav1' className='box'>
+                    <div className='box'>
+                        <img className="imgCar" src={car1} alt='Vehicle Poster'/>
+                    </div>
                 </div>   
                 
-                <div class='box'>
-                    <div id="personalFav" class='box'>Our Personal Favorites</div>
+                <div id='vehicleFav2Box'className='box'>
+                    <div id="personalFav" className='box'>Our Personal Favorites</div>
                     
-                    <div id='vehicleFav2' class='box'>
-                        Car 2
+                    <div id='vehicleFav2' className='box'>
+                        <div className='box'>
+                            <img className="imgCar" src={car2} alt='Vehicle Poster'/>
+                        </div>
                     </div>
                 </div>
-                <div id='vehicleFav3' class='box'>Car 3</div>
+                <div id='vehicleFav3' className='box'>
+                    <div className='box'>
+                        <img className="imgCar" src={car3} alt='Vehicle Poster'/>
+                    </div>
+                </div>
             </div>
             
-            <div id='box5' class='box'>
-                <div id= 'innerMenu1' class='box'>KnowUrLights</div>
+            <div id='box5' className='box'>
+                <div id= 'innerMenu1' className='box'>KnowUrLights</div>
             </div>
-
-            {/* <table class="box">
-                <tr>
-                    <td class="box">Img</td>
-                    <td class="box">This light means...</td>
-                    <td class="box">Img</td>
-                    <td class="box">This light means...</td>
-                </tr>
-                <tr>
-                    <td class="box">Img</td>
-                    <td class="box">This light means...</td>
-                    <td class="box">Img</td>
-                    <td class="box">This light means...</td>
-                </tr>
-                <tr>
-                    <td class="box">Img</td>
-                    <td class="box">This light means...</td>
-                    <td class="box">Img</td>
-                    <td class="box">This light means...</td>
-                </tr>
-            </table> */}
-
-            {/* <div id='box3' class='box'>
-                <div id= 'vehicleLight1' class='box'>
-                        Vehicle Light 1
-                    <div id= 'vehicleLight1' class='box'>
-                        Vehicle Light 1
-                    </div>
-                </div>
-                <div id='vehicleLight2' class='box'>
-                    Vehicle Light 2
-                </div>
-            </div> */}
-
             <LightsTable /> 
-            
-
+            <footer>
+                Website by Arturo Duran &copy;
+            </footer>
         </div>
-    </div>
+    // </div>
     )
 }
 
--- knowurcar.com/src/LightsTable.js
+++ knowurcar.com/src/LightsTable.js
@@ -2,56 +2,56 @@ import './lightsCSS.css';
 
 function LightsTable() {
     return(
-        <div class="table-container" role="table" aria-label="Destinations">
-            <div class="flex-table row " role="rowgroup">
-                <div class="flex-row first vertLn horzLn"  role="cell">
-                    <img class="imgLight" src="imgLightex.png" alt="lightImg"/>
+        <div className="table-container" role="table" aria-label="Destinations">
+            <div className="flex-table row " role="rowgroup">
+                <div className="flex-row first vertLn horzLn"  role="cell">
+                    <img className="imgLight" src="imgLightex.png" alt="lightImg"/>
                 </div>
-                <div class="flex-row second horzLn" role="cell"></div>
-                <div class="flex-row first2 vertLn2 horzLn"  role="cell">
-                    <img class="imgLight" src="imgLightex.png" alt="lightImg"/>
+                <div className="flex-row second horzLn" role="cell">Here will go the info about the light.</div>
+                <div className="flex-row first2 vertLn2 horzLn"  role="cell">
+                    <img className="imgLight" src="imgLightex.png" alt="lightImg"/>
                 </div>
-                <div class="flex-row second horzLn" role="cell"></div>
+                <div className="flex-row second horzLn" role="cell">Here will go the info about the light.</div>
             </div>
-            <div class="flex-table row" role="rowgroup">
-                <div class="flex-row first vertLn horzLn"  role="cell">
-                    <img class="imgLight" src="imgLightex.png" alt="lightImg"/>
+            <div className="flex-table row" role="rowgroup">
+                <div className="flex-row first vertLn horzLn"  role="cell">
+                    <img className="imgLight" src="imgLightex.png" alt="lightImg"/>
                 </div>
-                <div class="flex-row second horzLn" role="cell"></div>
-                <div class="flex-row first2 vertLn2 horzLn"  role="cell">
-                    <img class="imgLight" src="imgLightex.png" alt="lightImg"/>
+                <div className="flex-row second horzLn" role="cell">Here will go the info about the light.</div>
+                <div className="flex-row first2 vertLn2 horzLn"  role="cell">
+                    <img className="imgLight" src="imgLightex.png" alt="lightImg"/>
                 </div>
-                <div class="flex-row second horzLn" role="cell"></div>
+                <div className="flex-row second horzLn" role="cell">Here will go the info about the light.</div>
             </div>
-            <div class="flex-table row" role="rowgroup">
-                <div class="flex-row first vertLn horzLn"  role="cell">
-                    <img class="imgLight" src="imgLightex.png" alt="lightImg"/>
+            <div className="flex-table row" role="rowgroup">
+                <div className="flex-row first vertLn horzLn"  role="cell">
+                    <img className="imgLight" src="imgLightex.png" alt="lightImg"/>
                 </div>
-                <div class="flex-row second horzLn" role="cell"></div>
-                <div class="flex-row first2 vertLn2 horzLn"  role="cell">
-                    <img class="imgLight" src="imgLightex.png" alt="lightImg"/>
+                <div className="flex-row second horzLn" role="cell">Here will go the info about the light.</div>
+                <div className="flex-row first2 vertLn2 horzLn"  role="cell">
+                    <img className="imgLight" src="imgLightex.png" alt="lightImg"/>
                 </div>
-                <div class="flex-row second horzLn" role="cell"></div>
+                <div className="flex-row second horzLn" role="cell">Here will go the info about the light.</div>
             </div>
-            <div class="flex-table row" role="rowgroup">
-                <div class="flex-row first vertLn horzLn"  role="cell">
-                    <img class="imgLight" src="imgLightex.png" alt="lightImg"/>
+            <div className="flex-table row" role="rowgroup">
+                <div className="flex-row first vertLn horzLn"  role="cell">
+                    <img className="imgLight" src="imgLightex.png" alt="lightImg"/>
                 </div>
-                <div class="flex-row second horzLn" role="cell"></div>
-                <div class="flex-row first2 vertLn2 horzLn"  role="cell">
-                    <img class="imgLight" src="imgLightex.png" alt="lightImg"/>
+                <div className="flex-row second horzLn" role="cell">Here will go the info about the light.</div>
+                <div className="flex-row first2 vertLn2 horzLn"  role="cell">
+                    <img className="imgLight" src="imgLightex.png" alt="lightImg"/>
                 </div>
-                <div class="flex-row second horzLn" role="cell"></div>
+                <div className="flex-row second horzLn" role="cell">Here will go the info about the light.</div>
             </div>
-            <div class="flex-table row" role="rowgroup">
-                <div class="flex-row first vertLn"  role="cell">
-                    <img class="imgLight" src="imgLightex.png" alt="lightImg"/>
+            <div className="flex-table row" role="rowgroup">
+                <div className="flex-row first vertLn"  role="cell">
+                    <img className="imgLight" src="imgLightex.png" alt="lightImg"/>
                 </div>
-                <div class="flex-row second" role="cell"></div>
-                <div class="flex-row first2 vertLn2"  role="cell">
-                    <img class="imgLight" src="imgLightex.png" alt="lightImg"/>
+                <div className="flex-row second" role="cell">Here will go the info about the light.</div>
+                <div className="flex-row first2 vertLn2"  role="cell">
+                    <img className="imgLight" src="imgLightex.png" alt="lightImg"/>
                 </div>
-                <div class="flex-row second" role="cell"></div>
+                <div className="flex-row second" role="cell">Here will go the info about the light.</div>
             </div>
         </div>
     )
