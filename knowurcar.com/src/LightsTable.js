b6ce5bd6f6875d6044a7902ba73ca95da2089e6c
ArturoDuran90 <aduran@student.neumont.edu>
2022-07-14 12:58:13 -0600

    added more rows to lights

--- knowurcar.com/src/LightsTable.js
+++ knowurcar.com/src/LightsTable.js
@@ -44,6 +44,46 @@ function LightsTable() {
                 <div className="flex-row second horzLn" role="cell">Here will go the info about the light.</div>
             </div>
             <div className="flex-table row" role="rowgroup">
+                <div className="flex-row first vertLn horzLn"  role="cell">
+                    <img className="imgLight" src="imgLightex.png" alt="lightImg"/>
+                </div>
+                <div className="flex-row second horzLn" role="cell">Here will go the info about the light.</div>
+                <div className="flex-row first2 vertLn2 horzLn"  role="cell">
+                    <img className="imgLight" src="imgLightex.png" alt="lightImg"/>
+                </div>
+                <div className="flex-row second horzLn" role="cell">Here will go the info about the light.</div>
+            </div>
+            <div className="flex-table row" role="rowgroup">
+                <div className="flex-row first vertLn horzLn"  role="cell">
+                    <img className="imgLight" src="imgLightex.png" alt="lightImg"/>
+                </div>
+                <div className="flex-row second horzLn" role="cell">Here will go the info about the light.</div>
+                <div className="flex-row first2 vertLn2 horzLn"  role="cell">
+                    <img className="imgLight" src="imgLightex.png" alt="lightImg"/>
+                </div>
+                <div className="flex-row second horzLn" role="cell">Here will go the info about the light.</div>
+            </div>
+            <div className="flex-table row" role="rowgroup">
+                <div className="flex-row first vertLn horzLn"  role="cell">
+                    <img className="imgLight" src="imgLightex.png" alt="lightImg"/>
+                </div>
+                <div className="flex-row second horzLn" role="cell">Here will go the info about the light.</div>
+                <div className="flex-row first2 vertLn2 horzLn"  role="cell">
+                    <img className="imgLight" src="imgLightex.png" alt="lightImg"/>
+                </div>
+                <div className="flex-row second horzLn" role="cell">Here will go the info about the light.</div>
+            </div>
+            <div className="flex-table row" role="rowgroup">
+                <div className="flex-row first vertLn horzLn"  role="cell">
+                    <img className="imgLight" src="imgLightex.png" alt="lightImg"/>
+                </div>
+                <div className="flex-row second horzLn" role="cell">Here will go the info about the light.</div>
+                <div className="flex-row first2 vertLn2 horzLn"  role="cell">
+                    <img className="imgLight" src="imgLightex.png" alt="lightImg"/>
+                </div>
+                <div className="flex-row second horzLn" role="cell">Here will go the info about the light.</div>
+            </div>
+            <div className="flex-table row" role="rowgroup">
                 <div className="flex-row first vertLn"  role="cell">
                     <img className="imgLight" src="imgLightex.png" alt="lightImg"/>
                 </div>
