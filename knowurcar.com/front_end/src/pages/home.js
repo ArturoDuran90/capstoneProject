12e7c963912929b8d26b5ab784692e3dee0f316c
ArturoDuran90 <aduran@student.neumont.edu>
2022-07-29 06:29:04 -0600

    trying to fix routes

---
+++ knowurcar.com/front_end/src/pages/home.js
@@ -0,0 +1,169 @@
+import React from "react";
+
+const Home = () => {
+return (
+	<div id='insideContainer'>
+                <div className='box navBar2Text'>
+                    <p className="navBar2Text">Select your vehicle below for more information.</p>
+                </div>
+                <div id='navBar2' className='box'>
+                    <div className='box'>
+                        <CarFilter />
+                        {/* <input className='box vehicleInput' type='text' placeholder='Vehicle Year'/> */}
+                        {/* <CarMakes /> */}
+                        {/* <input className='box vehicleInput' type='text' placeholder='Vehicle Make'/> */}
+                        {/* <CarYears /> */}
+                        {/* <div id='LookUpDiv'> */}
+                        {/* <span id='lookUpBtn' onClick={() => {}}>
+                            Look up Model & Trim
+                        </span> */}
+                        {/* </div> */}
+                        {/* <input className='box vehicleInput' type='text' placeholder='Vehicle Model'/> */}
+                        {/* <br/> */}
+                        {/* <CarModels/> */}
+                        {/* <input className='box vehicleInput' type='text' placeholder='Vehicle Trim'/> */}
+                        {/* <CarTrims/> */}
+                    </div>
+                </div>
+                <div id='LookUpDiv'>
+                    {/* <div id='lookUpBtn' onClick={() => {}}>
+                        Look up Vehicle
+                    </div> */}
+                    <Router>
+											<Navbar />
+											<Routes>
+												<Route path='/' element={<Home/>} />
+												<Route path='/about' element={<About/>} />
+												<Route path='/contact' element={<Contact/>} />
+												<Route path='/blogs' element={<Blogs/>} />
+												<Route path='/sign-up' element={<SignUp/>} />
+											</Routes>
+										</Router>
+                </div>
+                <hr/>
+                <br/>
+                <div id='vehicleFavContainer' className='box'>
+                    <div id='vehicleFav1' className='box'>
+                    {
+                        favCars.car1?.map((car, index) => (
+                        <div className='box' id='car1'> 
+                            { car.carImage != 'N/A'?       
+                                <div>
+                                    <a>
+                                        <img className="imgCar" src={car.carImage} alt='Vehicle Poster'/>
+                                    </a>
+                                </div> : ''
+                            }
+                        </div>
+                    ))
+                    }
+
+                        <br/>
+                        <h3 className='box'>BMW 328i xDrive GT</h3>
+                        <VehicleFav1/>
+                        <div>
+                            {
+                                favCars.car1?.map((car, index) => (
+                                <div className='box' id='car1'> 
+                                    { car.carPDF != 'N/A'?       
+                                        <div>
+                                            <a href={car.carPDF} target="_blank">
+                                                <img src="http://localhost:4001/PdfImg.png" alt='PdfLogo'/>
+                                            </a>
+                                        </div> : ''
+                                    }
+                                </div>
+                                ))
+                            }
+
+                        </div>
+                    </div>
+                    <div id='vehicleFav2Box'className='box'>
+                        <div id="personalFav" className='box'>Our Personal Favorites</div>
+                        <div id='vehicleFav2' className='box'>
+                            {
+                                favCars.car2?.map((car, index) => (
+                                <div className='box' id='car2'> 
+                                    { car.carImage != 'N/A'?       
+                                        <div className='box'>
+                                            <a>
+                                                <img className="imgCar" src={car.carImage} alt='Vehicle Poster'/>
+                                            </a>
+                                        </div> : ''
+                                    }
+                                </div>
+                                ))
+                            }
+
+
+                            <h3 className='box'>Kia K5 GT-Line</h3>
+                            <VehicleFav2/>
+                            <div>
+                                {
+                                    favCars.car2?.map((car, index) => (
+                                    <div className='box' id='car2'> 
+                                        { car.carPDF != 'N/A'?       
+                                            <div>
+                                                <a href={car.carPDF} target="_blank">
+                                                    <img src="http://localhost:4001/PdfImg.png" alt='PdfLogo'/>
+                                                </a>
+                                            </div> : ''
+                                        }
+                                    </div>
+                                    ))
+                                }
+
+                            </div>
+                        </div>
+                    </div>
+                    <div id='vehicleFav3' className='box'>
+                        {
+                            favCars.car3?.map((car, index) => (
+                            <div className='box' id='car3'> 
+                                { car.carImage != 'N/A'?       
+                                    <div className='box'>
+                                        <a>
+                                            <img className="imgCar" src={car.carImage} alt='Vehicle Poster'/>
+                                        </a>
+                                    </div> : ''
+                                }
+                            </div>
+                            ))
+                        }
+
+                        <h3 className='box'>Honda Civic Type R</h3>
+                        <VehicleFav3/>
+                        <div>
+                                {
+                                    favCars.car3?.map((car, index) => (
+                                    <div className='box' id='car3'> 
+                                        { car.carPDF != 'N/A'?       
+                                            <div>
+                                                <a href={car.carPDF} target="_blank">
+                                                    <img src="http://localhost:4001/PdfImg.png" alt='PdfLogo'/>
+                                                </a>
+                                            </div> : ''
+                                        }
+                                    </div>
+                                    ))
+                                }
+                            </div>
+                        
+                        <div id='lightLink'></div>
+                    </div>
+                </div>
+                <hr/>
+                <br/>
+                <div id='lightsDiv' className='box'>
+                    <div id='innerMenu1' className='box'>KnowUrLights</div>
+                </div>
+                <LightsTable /> 
+                <footer>
+                    Website by Arturo Duran &copy;
+                </footer>
+                <br/>
+            </div>
+);
+};
+
+export default Home;
